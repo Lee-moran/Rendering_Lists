e9eef98e4a989e95db14c715cd6785c965aa3f0a
Lee-moran <leemoran06@hotmail.com>
2022-08-15 13:27:41 +0000

    clock.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import RenderingLists from "./components/RenderingLists";
+import Clock from './components/Clock';
 
 function App() {
   return (
     <div className="App">
       <RenderingLists />
+      <Clock/>
     </div>
   );
 }
